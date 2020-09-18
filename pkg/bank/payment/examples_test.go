b29e61a5235c56541fa3ea39fd573dc3108eddc3
Student <student@go.alif.academy>
2020-09-18 18:43:02 +0500

    Добавлен пакет payment и тесты к нему

---
+++ pkg/bank/payment/examples_test.go
@@ -0,0 +1,36 @@
+package payment
+import(
+	"fmt"
+	"bank/pkg/bank/types"	
+)
+func ExamplePaymentSources()  {
+	card := []types.Card{
+		types.Card{
+			PAN: "3006",
+			Balance: 32_000,
+			Active:  true,			
+		},
+
+		types.Card{
+			PAN: "4006",
+			Balance: 122_000,
+			Active:  true,			
+		},
+
+		types.Card{
+			PAN: "5006",
+			Balance: 42_000,
+			Active:  false,			
+		},
+	}
+
+	payCard := PaymentSources(card)
+	
+	for _, payCard := range payCard {
+		fmt.Println(payCard.Number)
+	}
+	
+	//Output: 
+	//3006 
+	//4006
+}
---
+++ pkg/bank/payment/payment.go
@@ -0,0 +1,21 @@
+package payment
+
+import(
+	"bank/pkg/bank/types"
+)
+
+func PaymentSources(cards []types.Card) []types.PaymentSources {
+	payCard := []types.PaymentSources{}
+
+	for _, cards := range cards {
+		if cards.Balance > 0 && cards.Active{
+			payCard = append(payCard, types.PaymentSources{				
+				cards.Balance,
+				string(cards.PAN),
+				},)									
+			}	
+		}	
+		return payCard		
+	}	
+
+	
